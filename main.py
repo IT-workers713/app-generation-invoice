b2341be9ca454b765147b91651837bb7e52d075f
IT-workers713 <rayehriad99@gmail.com>
2024-01-28 19:38:31 +0100

    create minim pdf from excel files

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+import os.path
+
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
+
 
 filepaths = glob.glob("invoices/*.xlsx")
 print(filepaths)
 
 for filepath in filepaths:
-    try:
-        df = pd.read_excel(filepath, sheet_name="Sheet 1")
-        print(df)
-    except Exception as e:
-        print(f"Error reading file {filepath}: {e}")
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    pdf = FPDF(orientation="P",format="A4",unit="mm")
+    pdf.add_page()
+    filename=Path(filepath).stem
+    invoice_nbr = filename.split("-")[0]
+    pdf.set_font(family="Times",size=16)
+    pdf.cell(w=5,h=8,txt=f"Facture Num.{invoice_nbr}  ")
+    pdf.output(f"PDFS/{filename}.pdf")
